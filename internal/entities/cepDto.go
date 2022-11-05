46bf812afc2ea87e635ed5653c175746eb733227
Gustavo Toporowicz <gustavotoporowicz@MacBook-Pro-de-Gustavo.local>
2022-11-05 20:24:30 -0300

    create internal folder

---
+++ internal/entities/cepDto.go
@@ -0,0 +1,12 @@
+package entities
+
+type CepDto struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Ddd         string `json:"ddd"`
+}
