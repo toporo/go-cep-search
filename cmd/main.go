dc8d86787d2a673c8956fc4c51617930fc6fca98
Gustavo Toporowicz <gustavotoporowicz@MacBook-Pro-de-Gustavo.local>
2022-11-06 02:00:38 -0300

    REMOVE unused code

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/labstack/echo/v4"
-	"github.com/rs/zerolog/log"
 
 	"github.com/toporo/go-cep-search/internal/api"
-	"github.com/toporo/go-cep-search/internal/entities"
 )
 
 var httpClient = &http.Client{Timeout: 10 * time.Second}
@@ -24,24 +21,4 @@ func main() {
 
 	err := e.Start(":8080")
 	fmt.Println(err.Error())
-}
-
-func FindCep(cep string) entities.CepDto {
-	cepObject := new(entities.CepDto)
-	getCep(cep, cepObject)
-
-	return *cepObject
-}
-
-func getCep(cep string, target interface{}) error {
-	log.Info().Msg("findCep: " + cep)
-
-	resp, err := httpClient.Get(fmt.Sprintf("http://viacep.com.br/ws/%s/json/", cep))
-	if err != nil {
-		log.Error().Err(err).Msg("Error findCep: " + cep)
-	}
-
-	defer resp.Body.Close()
-
-	return json.NewDecoder(resp.Body).Decode(target)
 }
