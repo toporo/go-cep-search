3670c571ccbc34ec1ff6db2e5a16cdf3a707dcec
Gustavo Toporowicz <gustavotoporowicz@MacBook-Pro-de-Gustavo.local>
2022-11-06 02:01:19 -0300

    REMOVE unused code

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
 
 	"github.com/toporo/go-cep-search/internal/api"
 )
-
-var httpClient = &http.Client{Timeout: 10 * time.Second}
 
 func main() {
 	e := echo.New()
